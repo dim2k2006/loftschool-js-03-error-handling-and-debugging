1a32f4e42ca81f6f30bda702ef5e2f96bc5bb511
Дмитрий Малофеев <dim2k2006@yandex.ru>
2016-07-10 17:28:17 +0300

    first commit

---
+++ homework#1/index.js
@@ -0,0 +1,40 @@
+var isAllTrue = function(source, filterFn) {
+    var sourceLength = source.length,
+        i = 0,
+        result = false;
+
+    try {
+
+        if (sourceLength === 0) {
+            throw new Error('Массив не должен быть пустым');
+        }
+
+        for (i; i < sourceLength; i++) {
+
+            result = filterFn(source[i]);
+
+            if (!result) {
+                break;
+            }
+
+        }
+
+    } catch(e) {
+
+        console.log(e);
+
+    }
+
+    return result;
+};
+
+var isNumber = function(val) {
+    return typeof val === 'number';
+};
+
+var allNumbers = [1, 2, 4, 5, 6, 7, 8],
+    someNumbers = [1, 2, 'привет', 4, 5, 'loftschool', 6, 7, 8],
+    noNumbers = ['это', 'массив', 'без', 'чисел'];
+
+
+console.log(isAllTrue(noNumbers, isNumber));
